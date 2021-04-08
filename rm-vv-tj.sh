788b526fd312c31dec21a448cf1ac72e8e88e048
dpSky <56128195+dpSky@users.noreply.github.com>
2021-04-08 10:50:47 +0800

    Update rm-vv-tj.sh

--- rm-vv-tj.sh
+++ rm-vv-tj.sh
@@ -2,10 +2,10 @@
 
 echo '开始移除旧服务'
 
-folder=$1-tj
+folder=$1
 
 #kill&remove old servers
-kill -9 $(ps -ef | grep ${folder} | grep -v grep | grep -v bash | awk '{print $1}') 1 > /dev/null
+kill -9 $(ps -ef | grep ${folder}-tj | grep -v grep | grep -v bash | awk '{print $1}') 1 > /dev/null
 kill -9 $(ps -ef | grep defunct | grep -v grep | awk '{print $1}') 1 > /dev/null
 systemctl stop trojan-go.service
 systemctl disable trojan-go.service
@@ -13,7 +13,7 @@ systemctl stop vvlink-tj.service
 systemctl disable vvlink-tj.service
 rm -f /etc/systemd/system/trojan-go.service
 rm -f /etc/systemd/system/vvlink-tj.service
-rm -rf $folder
+rm -rf $folder-tj
 
 echo '旧服务已移除'
 sleep 3

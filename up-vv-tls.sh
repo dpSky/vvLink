b29310f2749d1d4387f95cc34d761079ad5631a7
dpSky <56128195+dpSky@users.noreply.github.com>
2021-10-27 21:13:42 +0800

    Update up-vv-tls.sh

--- up-vv-tls.sh
+++ up-vv-tls.sh
@@ -6,7 +6,7 @@ wget https://dpsky.cc/vvlink-a07wm6/vvlink.crt
 rm -rf /root/.cert/server.*
 cp vvlink.crt /root/.cert/server.crt
 cp vvlink.key /root/.cert/server.key
-chmod 400 /root/.cert/server.*
+chmod 600 /root/.cert/server.*
 echo '证书更新完成 等待重启'
 sleep 3
 reboot
